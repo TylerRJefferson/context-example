a506d61ae32c4d418c2eb0a86c0e0c787eb4bd4f
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-11-03 11:43:46 -0400

    using context for login/logout buttons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,10 @@
+import Logout from "./Logout";
+
 export default function Footer() {
   return (
     <footer>
       <p>Footer</p>
+      <Logout />
     </footer>
   )
 }
--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -1,7 +1,10 @@
+import Login from "./Login"
+
 export default function MenuBar() {
   return (
     <nav>
       <p>Menu Bar</p>
+      <Login />
     </nav>
   )
 }
